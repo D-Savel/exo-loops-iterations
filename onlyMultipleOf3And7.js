67e17d34b0f2c5e098027c358219e458db9f9b93
D-Savel <dad.savel@gmail.com>
2021-03-03 10:25:13 +0100

    update code onlyMultipleOf3And7

--- onlyMultipleOf3And7.js
+++ onlyMultipleOf3And7.js
@@ -1,4 +1,3 @@
-for (let index = 0; index <= 1000; index++) {
-  index%3 === 0 ? console.log(`${index} est un multiple de 3`) : ''
-  index%7 === 0 ? console.log(`${index} est un multiple de 7`) : ''
+for (let index = 1; index <= 1000; index++) {
+  index % 3 === 0 && index % 7 === 0 ? console.log(`${index} est un multiple de 3 et de 7`) : ''
 }
